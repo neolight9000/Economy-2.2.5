c5a8e4b45e04fc850bd32a5fb4679c1dc63b5aa2
NeoLight <50766362+neolight9000@users.noreply.github.com>
2021-01-23 14:26:45 +0200

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyTitle("Economy")]
+[assembly: AssemblyDescription("")]
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Economy")]
+[assembly: AssemblyCopyright("NeoLight 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("794f0931-4521-461a-b072-1a0da7fec357")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+[assembly: AssemblyVersion("1.2.0.0")]
